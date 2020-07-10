f78dcbbfd8a7cf0a4674fcd7442af9ba87d1aad9
Mariano <marianob85work@gmail.com>
2020-07-10 17:58:26 +0200

    clean up

--- Make.cmd
+++ Make.cmd
@@ -1,6 +1,6 @@
 py -m venv env
-rem call venv\Scripts\activate.bat
-.\env\Scripts\activate
+call .\env\Scripts\activate
 pip install -r requirements.txt
 python ChannelConverter.py -r Channels -l "d:\Source\SvnDocuments\Settings\ChannelLists\Enigma2\lamedb" -o d:\Temp\DVBViewer.ini -s
+call .\env\Scripts\deactivate
 pause
